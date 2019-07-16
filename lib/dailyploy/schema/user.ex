c76fc5408c606bfae3dfe514fceef8503b49e676
Vikram Singh Jadon <vikramjadon921995@gmail.com>
2019-07-16 13:41:34 +0530

    Added user migration and its changeset

--- lib/dailyploy/schema/user.ex
+++ lib/dailyploy/schema/user.ex
@@ -1,2 +1,22 @@
 defmodule DailyPloy.Schema.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias DailyPloy.Model.User
+
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :password, :password_confirmation])
+  end
 end
---
+++ priv/repo/migrations/20190716074547_create_users.exs
@@ -0,0 +1,13 @@
+defmodule Dailyploy.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+      add :password_hash, :string
+      timestamps()
+    end
+
+  end
+end
