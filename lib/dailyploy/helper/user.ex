09b3b9881709fd3ac14310f59e6c7cc5f7b9b2aa
Vikram Singh Jadon <vikramjadon921995@gmail.com>
2019-07-19 15:51:19 +0530

    Added company and user creation in multi

--- lib/dailyploy/helper/user.ex
+++ lib/dailyploy/helper/user.ex
@@ -5,6 +5,27 @@ defmodule Dailyploy.Helper.User do
   alias Dailyploy.Schema.User
   alias Dailyploy.Schema.Company
 
+  alias Dailyploy.Repo
+
   def create_user_with_company(user_attrs) do
+    case user_attrs_has_company_key?(user_attrs) do
+      true ->
+        %{company: company_data} = user_attrs
+        user_changeset = User.changeset(%User{}, user_attrs)
+        company_changeset = Company.changeset(%Company{}, company_data)
+        Repo.transaction multi_for_user_and_company_creation(user_changeset, company_changeset)
+      false ->
+        nil
+    end
+  end
+
+  defp user_attrs_has_company_key?(user_attrs) do
+    true
+  end
+
+  defp multi_for_user_and_company_creation(user_changeset, company_changeset) do
+    Multi.new
+    |> Multi.insert(:user, user_changeset)
+    |> Multi.insert(:company, company_changeset)
   end
 end
