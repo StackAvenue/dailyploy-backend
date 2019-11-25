881b33a910b9677a65759a98004b16a6d4598c94
humblefool <ayoush@stack-avenue.com>
2019-11-25 07:28:18 +0530

    heroku deployement

--- lib/dailyploy_web/channels/user_socket.ex
+++ lib/dailyploy_web/channels/user_socket.ex
@@ -4,7 +4,7 @@ defmodule DailyployWeb.UserSocket do
   ## Channels
   # channel "room:*", DailyployWeb.RoomChannel
   transport :websocket, Phoenix.Transports.WebSocket,
-    timeout: 65_000
+    timeout: 45_000
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
   # verification, you can put default assigns into
