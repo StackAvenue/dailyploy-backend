ea9da3278c0fe0f916bb66e5f69785be1385b242
Chandra Shekhar <chandra@dealsignal.com>
2019-11-29 16:06:24 +0530

    remove ssl true option

--- config/dev.exs
+++ config/dev.exs
@@ -8,8 +8,7 @@ config :dailyploy, Dailyploy.Repo,
   database: "dailyploy_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10,
-  ssl: true
+  pool_size: 10
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
