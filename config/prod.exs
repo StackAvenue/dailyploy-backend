6f307a6dd8b44eaba444add463399de0b368c3af
Vikram Singh Jadon <vikramjadon921995@gmail.com>
2019-12-07 12:43:28 +0530

    Set ssl configuration to false for prod

--- config/prod.exs
+++ config/prod.exs
@@ -23,11 +23,11 @@ config :dailyploy, Dailyploy.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  ssl: false
 
 config :sendgrid,
   api_key: System.get_env("SENDGRID_API_KEY")
-  
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
