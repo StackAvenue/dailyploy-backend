98da00ca6272d87f985cfaa3d470fccb173dd90d
Ayoush <ayoush@Ayoushs-MacBook-Air.local>
2020-02-13 11:07:13 +0530

    firebase added

--- lib/dailyploy/helper/firebase.ex
+++ lib/dailyploy/helper/firebase.ex
@@ -1,11 +1,13 @@
 defmodule Dailyploy.Helper.Firebase do
-    def insert_operation({:ok, json_data}, prefix) do
-        Task.async(fn ->
-          insert_into_firebase(json_data, prefix)
-        end)
-      end
-    
-      defp insert_into_firebase(json_data, prefix) do
-        Tesla.post!("https://dailyploy-test.firebaseio.com/#{prefix}.json", json_data, headers: [{"content-type", "application/json"}])
-      end
+  def insert_operation({:ok, json_data}, prefix) do
+    Task.async(fn ->
+      insert_into_firebase(json_data, prefix)
+    end)
+  end
+
+  defp insert_into_firebase(json_data, prefix) do
+    Tesla.post!("https://dailyploy-test.firebaseio.com/#{prefix}.json", json_data,
+      headers: [{"content-type", "application/json"}]
+    )
+  end
 end
