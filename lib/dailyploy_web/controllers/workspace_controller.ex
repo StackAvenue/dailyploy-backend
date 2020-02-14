c22c67b48f69b12d019b4105f65b7efdce51dfa9
Ayoush <ayoush@Ayoushs-MacBook-Air.local>
2020-02-14 17:53:48 +0530

    Added and tested user task now it is coming as planned but don't know about how it is going to act on production so currently deploying it on test environment

--- lib/dailyploy_web/controllers/workspace_controller.ex
+++ lib/dailyploy_web/controllers/workspace_controller.ex
@@ -159,26 +159,25 @@ defmodule DailyployWeb.WorkspaceController do
           user.tasks
           |> Enum.reduce(%{}, fn task, acc ->
             [range_end_date, range_start_date] =
-              if(
-                Enum.empty?(task.time_tracks) and
-                  (Date.diff(task.start_datetime, start_date) >= 0 and
-                     Date.diff(task.end_datetime, end_date) <= 0)
-              ) do
+              if(Enum.empty?(task.time_tracks)) do
                 range_end_date = smaller_date(DateTime.to_date(task.end_datetime), end_date)
                 range_start_date = greater_date(DateTime.to_date(task.start_datetime), start_date)
                 [range_end_date, range_start_date]
               else
-                if(!Enum.empty?(task.time_tracks)) do
-                  fist_time_track = task.time_tracks |> List.first()
-                  last_time_track = task.time_tracks |> List.last()
+                first_time_track = task.time_tracks |> List.first()
+                last_time_track = task.time_tracks |> List.last()
 
-                  task_start_date = smaller_date(task.start_datetime, fist_time_track.start_time)
-                  task_end_date = greater_date(task.end_datetime, last_time_track.start_time)
+                task_start_date =
+                  smaller_date(task.start_datetime, first_time_track.start_time)
+                  |> DateTime.to_date()
 
-                  range_end_date = smaller_date(task_start_date, end_date) |> DateTime.to_date()
-                  range_start_date = greater_date(task_end_date, start_date) |> DateTime.to_date()
-                  [range_end_date, range_start_date]
-                end
+                task_end_date =
+                  greater_date(task.end_datetime, last_time_track.start_time)
+                  |> DateTime.to_date()
+
+                range_start_date = smaller_date(task_start_date, end_date)
+                range_end_date = greater_date(task_end_date, start_date)
+                [range_end_date, range_start_date]
               end
 
             date_formatted_time_tracks =
@@ -228,7 +227,17 @@ defmodule DailyployWeb.WorkspaceController do
 
               if(
                 Enum.member?(is_time_track_present, true) or
-                  Date.diff(task.start_datetime, date) === 0 or Enum.empty?(task.time_tracks)
+                  Date.diff(task.start_datetime, date) === 0 or
+                  Enum.empty?(task.time_tracks) or
+                  (Date.diff(range_end_date, task.end_datetime) >= 0 and
+                     Date.diff(range_start_date, task.start_datetime) <= 0 and
+                     Enum.member?(
+                       Date.range(
+                         DateTime.to_date(task.start_datetime),
+                         DateTime.to_date(task.end_datetime)
+                       ),
+                       date
+                     ))
               ) do
                 tasks = Map.get(date_acc, Date.to_iso8601(date)) ++ [task]
                 Map.put(date_acc, Date.to_iso8601(date), tasks)
