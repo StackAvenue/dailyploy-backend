aca6edea08cc8f6bb16a8f845472275ae859adfb
Ayoush <ayoush@Ayoushs-MacBook-Air.local>
2020-02-20 17:55:37 +0530

    firebase something changes

--- lib/dailyploy/helper/firebase.ex
+++ lib/dailyploy/helper/firebase.ex
@@ -6,7 +6,7 @@ defmodule Dailyploy.Helper.Firebase do
   end
 
   defp insert_into_firebase(json_data, prefix) do
-    Tesla.post!("https://dailyploy-56283.firebaseio.com/#{prefix}.json", json_data,
+    Tesla.post!("https://dailyploy-test.firebaseio.com/#{prefix}.json", json_data,
       headers: [{"content-type", "application/json"}]
     )
   end
