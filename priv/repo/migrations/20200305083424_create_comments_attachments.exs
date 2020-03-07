0596d646f3f13f1ce8c4f335abb735c0464e42fe
Ayoush <ayoush@Ayoushs-MacBook-Air.local>
2020-03-07 15:18:53 +0530

    modified migration a little

--- priv/repo/migrations/20200305083424_create_comments_attachments.exs
+++ priv/repo/migrations/20200305083424_create_comments_attachments.exs
@@ -4,7 +4,7 @@ defmodule Dailyploy.Repo.Migrations.CreateCommentsAttachments do
   def change do
     create table(:comments_attachments) do
       add :task_id, references(:users, on_delete: :delete_all)
-      add :comment_id, references(:task_comments, on_delete: :delete_all)
+      add :task_comment_id, references(:task_comments, on_delete: :delete_all)
       add :image_url, :string
 
       timestamps()
