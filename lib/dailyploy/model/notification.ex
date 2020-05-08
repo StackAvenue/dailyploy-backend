81338ea6650636de375982baad8aa2e2f1edc9f2
BandanaPandey <bandanapandey11@gmail.com>
2020-05-09 01:19:14 +0530

    Added Notification Model, to create, list and update notifications

---
+++ lib/dailyploy/model/notification.ex
@@ -0,0 +1,45 @@
+defmodule Dailyploy.Model.Notification do
+  alias Dailyploy.Repo
+  alias Dailyploy.Schema.Notification
+  import Ecto.Query
+
+  @doc """
+  Fetch a particular Notification
+  """
+  def get(id) do
+    Repo.get(Notification, id)
+  end
+
+  @doc """
+  Fetch all Notifications
+  """
+  def get_all() do
+    Repo.all(Notification)
+  end
+
+  @doc """
+  Fetch all unreaded Notifications
+  """
+  def get_unreads() do
+    Notification
+    |> where([notification], notification.read == ^false)
+    |> Repo.all()
+  end
+
+  @doc """
+  Creates Notification
+  """
+
+  def create(params) do
+    changeset = Notification.changeset(%Notification{}, params)
+    Repo.insert(changeset)
+  end
+
+  @doc """
+  Updates Notification
+  """
+  def update(notification, params) do
+    changeset = Notification.changeset(notification, params)
+    Repo.update(changeset)
+  end
+end
