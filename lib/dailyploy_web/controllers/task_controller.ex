d1525a6d6c545df0be4939f80fad3c380bd156bf
BandanaPandey <bandanapandey11@gmail.com>
2020-05-09 16:51:16 +0530

    Implemented notifications create logic for task deletion

--- lib/dailyploy_web/controllers/task_controller.ex
+++ lib/dailyploy_web/controllers/task_controller.ex
@@ -36,7 +36,7 @@ defmodule DailyployWeb.TaskController do
 
     case TaskModel.create_task(task_params) do
       {:ok, %TaskSchema{} = task} ->
-        task = task |> Repo.preload([:project, :owner, :category, :time_tracks])
+        task = task |> Repo.preload([:members, :project, :owner, :category, :time_tracks])
 
         Firebase.insert_operation(
           Poison.encode(task),
@@ -72,7 +72,7 @@ defmodule DailyployWeb.TaskController do
 
     case TaskModel.update_task_status(task, task_params) do
       {:ok, %TaskSchema{} = task} ->
-        task = task |> Repo.preload([:project, :owner, :category, :time_tracks])
+        task = task |> Repo.preload([:members, :project, :owner, :category, :time_tracks])
 
         Firebase.insert_operation(
           Poison.encode(task),
@@ -169,7 +169,7 @@ defmodule DailyployWeb.TaskController do
     user = Guardian.Plug.current_resource(conn)
     task = TaskModel.get_task!(id)
 
-    task_copy = task |> Repo.preload([:members, :project])
+    task_copy = task |> Repo.preload([:members, :project, :owner])
 
     with false <- is_nil(task) do
       if user.id == task.owner_id do
